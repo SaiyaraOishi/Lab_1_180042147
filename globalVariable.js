77cb1322d2929dc2c9af2f2e1a4eb051ff89bef9
SaiyaraOishi <saiyara.oishi@gmail.com>
2021-04-30 12:06:04 +0600

    Running some globalVariables

---
+++ globalVariable.js
@@ -0,0 +1,5 @@
+//dirname
+const currentDirectory=__dirname;
+console.log(currentDirectory);
+
+console.log(__filename);
--- intro.js
+++ intro.js
@@ -1,1 +1,8 @@
-//just a test
+let amount=12;
+
+if (amount<10){
+    console.log("Not Okay!");
+}
+else{
+    console.log("Okay!");
+}
